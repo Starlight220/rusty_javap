b2102fedc1d534d2fb51110ef819a2053c6f25ee
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2024-07-19 12:33:06 +0300

    chmod scripts

--- data/Example.java
+++ data/Example.java
@@ -2,11 +2,21 @@ public class Example {
   private int sum;
   final long l = 32L;
   float f = 1.6f;
+
+  Example() {
+    System.err.println("ctor");
+  }
+
+  static void init() {
+    var example = new Example();
+  }
+
   public int example(int a, int b) {
     int c = a + b;
     sum = c;
     return c;
   }
+
   protected String exampleStr() {
     return String.format("%f", 3.1 + f);
   }
