6bd7de7885c8e5c3a2d8a30a221a9b2054095034
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2025-03-13 14:49:20 +0200

    add json2class2json test

---
+++ tests/json2class2json.rs
@@ -0,0 +1,13 @@
+use rusty_javap::model::class::Class;
+
+#[test]
+fn json2class2json() {
+    let class_json_content = include_bytes!("./Example.class.json");
+    let json_class: serde_json::Value = serde_json::from_slice(class_json_content).unwrap();
+
+    let class: Class = serde_json::from_value(json_class.clone()).unwrap();
+
+    let written_json: serde_json::Value = serde_json::to_value(class).unwrap();
+    assert_eq!(json_class, written_json);
+}
+
